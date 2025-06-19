2107648785f2ac20383e4bc493bfb05797ed2753
asm2212 <asmareadmasu0@gmail.com>
2025-06-19 11:43:53 +0300

    get feeds follows

--- handler_feed_f0llows.go
+++ handler_feed_f0llows.go
@@ -35,3 +35,13 @@ func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.
 	respondWithJSON(w, 201, databaseFeedFollowToFeedFollow(feedFollow))
 
 }
+
+func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Request, user *database.User) {
+	feedFollows, err := apiCfg.DB.GetFeedFollows(r.Context(), user.ID)
+	if err != nil {
+		respondWithError(w, 400, "could not get feed follows: "+err.Error())
+		return
+	}
+	respondWithJSON(w, 201, databaseFeedFollowsToFeedFollows(feedFollows))
+
+}
